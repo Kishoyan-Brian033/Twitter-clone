10cb744d3cb2cb07c01178c104bfb400b9a1b195
Brian Mwangi <brianmwangiat033@gmail.com>
2025-05-25 16:56:31 +0300

    Html & css

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+
+
+const userUrl = 'https://jsonplaceholder.typicode.com/users ';
+const postUrl = 'https://jsonplaceholder.typicode.com/posts ';
+const commentsUrl = 'https://jsonplaceholder.typicode.com/comments '
+
+const userSelect = document.getElementById('userSelect') as HTMLSelectElement;
+const postsContainer = document.getElementById('postsContainer') as HTMLDivElement;
