a3f01c34fb7addcd432e224ef9f48530fafc2cbc
Brian Mwangi <brianmwangiat033@gmail.com>
2025-05-26 12:22:07 +0300

    change the project design

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,44 @@
 "use strict";
+// interface User {
+//   id: number;
+//   name: string;
+//   email: string;
+// }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+// async function getUsers(): Promise<void> {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const data: User[] = await response.json();
+//     data.forEach(user => {
+//       console.log(`Name: ${user.name}, Email: ${user.email}`);
+//     });
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//   }
+// }
+// getUsers();
+function getData() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const url = "https://jsonplaceholder.typicode.com/users";
+        try {
+            const response = yield fetch(url);
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+            const json = yield response.json();
+            console.log(json);
+        }
+        catch (error) {
+            console.error(Error);
+        }
+    });
+}
+getData();
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,43 @@
 
 
-const userUrl = 'https://jsonplaceholder.typicode.com/users ';
-const postUrl = 'https://jsonplaceholder.typicode.com/posts ';
-const commentsUrl = 'https://jsonplaceholder.typicode.com/comments '
+// interface User {
+//   id: number;
+//   name: string;
+//   email: string;
+// }
 
-const userSelect = document.getElementById('userSelect') as HTMLSelectElement;
-const postsContainer = document.getElementById('postsContainer') as HTMLDivElement;
+// async function getUsers(): Promise<void> {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const data: User[] = await response.json();
+
+
+//     data.forEach(user => {
+//       console.log(`Name: ${user.name}, Email: ${user.email}`);
+//     });
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//   }
+// }
+
+// getUsers();
+
+
+
+
+async function getData() {
+  const url = "https://jsonplaceholder.typicode.com/users";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(Error);
+  }
+}
+
+getData()
